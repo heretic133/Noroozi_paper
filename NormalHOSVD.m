d22adc82fb7cedaccddd96d4cf66fb498194fb1b
MansoorCompu <mansoor.re@gmail.com>
2019-06-24 17:05:16 +0430

    matlab for class

---
+++ NormalHOSVD.m
@@ -0,0 +1,30 @@
+index=5;
+emci1=emci;
+emci1(:,:,index)=[];
+normal1=normal;
+normal1(:,:,end+1)=emci(:,:,index);
+
+E=tensor(emci);
+E1=tensor(emci1);
+N=tensor(normal);
+N1=tensor(normal1);
+[C1,Ue1,S1]=hosvd(N1,[130,116,38]);
+[C,Ue1,S1]=hosvd(N,[130,116,38]);
+[Ce,Ue1,S1]=hosvd(E,[130,116,43]);
+[Ce1,Ue1,S1]=hosvd(E1,[130,116,43]);
+
+for i=1:38
+Tempn=double(C(:,:,i));
+Tempn1=double(C1(:,:,i));
+n(i)=Tempn(:)'*Tempn1(:)/(norm(Tempn,'fro')*norm(Tempn1,'fro'));
+end;
+
+for i=1:43
+
+Tempe=double(Ce(:,:,i));
+Tempe1=double(Ce1(:,:,i));
+e(i)=Tempe(:)'*Tempe1(:)/(norm(Tempe,'fro')*norm(Tempe1,'fro'));
+end;
+plot(n,'-*')
+hold on
+plot(e,'-o')
